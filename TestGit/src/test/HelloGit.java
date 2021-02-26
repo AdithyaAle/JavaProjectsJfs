5089a15e85ba5ce04bfe16b3fce33ab131af8959
Adithya <Adithya@LAPTOP-4LD9O7SU>
2021-02-26 09:15:10 +0530

    new commit

---
+++ TestGit/src/test/HelloGit.java
@@ -0,0 +1,9 @@
+package test;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		int x=200;
+		System.out.println("x = "+ x);
+		System.out.println("Hello this is a sample.");
+	}
+}
---
+++ TestGit/src/test/TestClass.java
@@ -0,0 +1,8 @@
+package test;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("new class"); 
+		System.out.println("Updated in TestClass");
+	}
+}
